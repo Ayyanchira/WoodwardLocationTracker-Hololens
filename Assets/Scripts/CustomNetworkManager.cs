7cca2a4ea84a753143c6d611455511cc895d4f47
Ayyanchira <ayyanchira.akshay@gmail.com>
2018-04-11 22:31:56 -0400

    Implemented Custom Network Manager
    Copied Custom Manager of previous project but trying to keep the camera of server unchanged.

---
+++ Assets/Scripts/CustomNetworkManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CustomNetworkManager : NetworkManager {
+
+    public GameObject playerPrefab;
+    public void connectAsClient()
+    {
+        NetworkManager.singleton.StartClient();
+    }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        short playerCount = (System.Int16)conn.playerControllers.Count;
+        ClientScene.AddPlayer(conn, playerCount);
+        base.OnClientConnect(conn);
+    }
+}
